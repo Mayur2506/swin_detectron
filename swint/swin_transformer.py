ecf8bc4c1c198f23453e098ea830d129b0edd936
Mayur2506 <mayur.kongutte2002@gmail.com>
2024-04-09 23:02:49 +0530

    changes

--- swint/swin_transformer.py
+++ swint/swin_transformer.py
@@ -337,10 +337,11 @@ class SwinTransformerBlock(nn.Module):
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
-
+        self.H = None
+        self.W = None
 
     def forward(self, x,mask_matrix):
-        H, W = self.input_resolution
+        H, W = self.H, self.W
         B, L, C = x.shape
         assert L == H * W, "input feature has wrong size %d, %d, %d" % (L, H, W)
 
@@ -372,26 +373,6 @@ class SwinTransformerBlock(nn.Module):
         x = x + self.drop_path(self.mlp(self.norm2(x)))
 
         return x
-
-    def extra_repr(self) -> str:
-        return f"dim={self.dim}, input_resolution={self.input_resolution}, num_heads={self.num_heads}, " \
-               f"group_size={self.group_size}, lsda_flag={self.lsda_flag}, mlp_ratio={self.mlp_ratio}"
-
-    def flops(self):
-        flops = 0
-        H, W = self.input_resolution
-        # norm1
-        flops += self.dim * H * W
-        # LSDA
-        nW = H * W / self.group_size / self.group_size
-        flops += nW * self.attn.flops(self.group_size * self.group_size)
-        # mlp
-        flops += 2 * H * W * self.dim * self.dim * self.mlp_ratio
-        # norm2
-        flops += self.dim * H * W
-        return flops
-
-
 
 
 
