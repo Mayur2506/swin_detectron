da9847cefca151eb60d368b3adc9c9a3571ce44c
Mayur2506 <mayur.kongutte2002@gmail.com>
2024-04-13 13:41:35 +0530

    changes

--- swint/swin_transformer.py
+++ swint/swin_transformer.py
@@ -230,9 +230,9 @@ class SwinTransformerBlock(nn.Module):
             H, W: Spatial resolution of the input feature.
             mask_matrix: Attention mask for cyclic shift.
         """
-        window_size_pred = self.window_size_predictor(x.mean(dim=(1, 2)))
-        window_sizes = torch.clamp(torch.round(window_size_pred.squeeze(1)), min=1, max=21).int()
-        self.window_size=torch.max(window_sizes).item()
+        # window_size_pred = self.window_size_predictor(x.mean(dim=(1, 2)))
+        # window_sizes = torch.clamp(torch.round(window_size_pred.squeeze(1)), min=1, max=21).int()
+        self.window_size=8
         B, L, C = x.shape
         H, W = self.H, self.W
         assert L == H * W, "input feature has wrong size"
