e57b03adcc94dcfdcad12924aecdb01e943ec0c5
Mayur2506 <mayur.kongutte2002@gmail.com>
2024-04-21 14:32:51 +0530

    backup part

--- swint/swin_transformer.py
+++ swint/swin_transformer.py
@@ -101,31 +101,41 @@ class WindowAttention(nn.Module):
         proj_drop (float, optional): Dropout ratio of output. Default: 0.0
     """
 
-    def __init__(self, dim, window_size, num_heads, qkv_bias=True, qk_scale=None, attn_drop=0., proj_drop=0.):
-
+    def __init__(self, dim, num_heads, window_size, global_window_size, qkv_bias=True, qk_scale=None, attn_drop=0., proj_drop=0.):
         super().__init__()
         self.dim = dim
-        self.window_size = window_size  # Wh, Ww
         self.num_heads = num_heads
-        head_dim = dim // num_heads
-        self.scale = qk_scale or head_dim ** -0.5
+        self.window_size = window_size
+        self.global_window_size = global_window_size
+        self.scale = qk_scale or (dim // num_heads) ** -0.5
 
-        # define a parameter table of relative position bias
         self.relative_position_bias_table = nn.Parameter(
-            torch.zeros((2 * window_size[0] - 1) * (2 * window_size[1] - 1), num_heads))  # 2*Wh-1 * 2*Ww-1, nH
+            torch.zeros((2 * window_size[0] - 1) * (2 * window_size[1] - 1) + (2 * global_window_size[0] - 1) * (2 * global_window_size[1] - 1), num_heads))
 
-        # get pair-wise relative position index for each token inside the window
-        coords_h = torch.arange(self.window_size[0])
-        coords_w = torch.arange(self.window_size[1])
-        coords = torch.stack(torch.meshgrid([coords_h, coords_w]))  # 2, Wh, Ww
-        coords_flatten = torch.flatten(coords, 1)  # 2, Wh*Ww
-        relative_coords = coords_flatten[:, :, None] - coords_flatten[:, None, :]  # 2, Wh*Ww, Wh*Ww
-        relative_coords = relative_coords.permute(1, 2, 0).contiguous()  # Wh*Ww, Wh*Ww, 2
-        relative_coords[:, :, 0] += self.window_size[0] - 1  # shift to start from 0
-        relative_coords[:, :, 1] += self.window_size[1] - 1
-        relative_coords[:, :, 0] *= 2 * self.window_size[1] - 1
-        relative_position_index = relative_coords.sum(-1)  # Wh*Ww, Wh*Ww
-        self.register_buffer("relative_position_index", relative_position_index)
+        coords_h_local = torch.arange(self.window_size[0])
+        coords_w_local = torch.arange(self.window_size[1])
+        coords_local = torch.stack(torch.meshgrid([coords_h_local, coords_w_local]))
+        coords_local_flatten = torch.flatten(coords_local, 1)
+        relative_coords_local = coords_local_flatten[:, :, None] - coords_local_flatten[:, None, :]
+        relative_coords_local = relative_coords_local.permute(1, 2, 0).contiguous()
+        relative_coords_local[:, :, 0] += self.window_size[0] - 1
+        relative_coords_local[:, :, 1] += self.window_size[1] - 1
+        relative_coords_local[:, :, 0] *= 2 * self.window_size[1] - 1
+        relative_position_index_local = relative_coords_local.sum(-1)
+
+        coords_h_global = torch.arange(self.global_window_size[0])
+        coords_w_global = torch.arange(self.global_window_size[1])
+        coords_global = torch.stack(torch.meshgrid([coords_h_global, coords_w_global]))
+        coords_global_flatten = torch.flatten(coords_global, 1)
+        relative_coords_global = coords_global_flatten[:, :, None] - coords_global_flatten[:, None, :]
+        relative_coords_global = relative_coords_global.permute(1, 2, 0).contiguous()
+        relative_coords_global[:, :, 0] += self.global_window_size[0] - 1
+        relative_coords_global[:, :, 1] += self.global_window_size[1] - 1
+        relative_coords_global[:, :, 0] *= 2 * self.global_window_size[1] - 1
+        relative_position_index_global = relative_coords_global.sum(-1)
+
+        self.register_buffer("relative_position_index_local", relative_position_index_local)
+        self.register_buffer("relative_position_index_global", relative_position_index_global)
 
         self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
         self.attn_drop = nn.Dropout(attn_drop)
@@ -136,37 +146,42 @@ class WindowAttention(nn.Module):
         self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, x, mask=None):
-        """ Forward function.
-        Args:
-            x: input features with shape of (num_windows*B, N, C)
-            mask: (0/-inf) mask with shape of (num_windows, Wh*Ww, Wh*Ww) or None
-        """
         B_, N, C = x.shape
         qkv = self.qkv(x).reshape(B_, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
-        q, k, v = qkv[0], qkv[1], qkv[2]  # make torchscript happy (cannot use tensor as tuple)
+        q, k, v = qkv[0], qkv[1], qkv[2]
 
         q = q * self.scale
-        attn = (q @ k.transpose(-2, -1))
+        attn_local = (q @ k.transpose(-2, -1))
+        attn_global = torch.matmul(q.transpose(1, 2), k.transpose(1, 2).transpose(-2, -1))
 
-        relative_position_bias = self.relative_position_bias_table[self.relative_position_index.view(-1)].view(
-            self.window_size[0] * self.window_size[1], self.window_size[0] * self.window_size[1], -1)  # Wh*Ww,Wh*Ww,nH
-        relative_position_bias = relative_position_bias.permute(2, 0, 1).contiguous()  # nH, Wh*Ww, Wh*Ww
-        attn = attn + relative_position_bias.unsqueeze(0)
+        relative_position_bias_local = self.relative_position_bias_table[:self.window_size[0] * self.window_size[1] ** 2, :].view(
+            self.window_size[0] * self.window_size[1], self.window_size[0] * self.window_size[1], -1)
+        relative_position_bias_local = relative_position_bias_local.permute(2, 0, 1).contiguous()
+        attn_local = attn_local + relative_position_bias_local.unsqueeze(0)
+
+        relative_position_bias_global = self.relative_position_bias_table[self.window_size[0] * self.window_size[1] ** 2:, :].view(
+            self.global_window_size[0] * self.global_window_size[1], self.global_window_size[0] * self.global_window_size[1], -1)
+        relative_position_bias_global = relative_position_bias_global.permute(2, 0, 1).contiguous()
+        attn_global = attn_global + relative_position_bias_global.unsqueeze(0)
 
         if mask is not None:
             nW = mask.shape[0]
-            attn = attn.view(B_ // nW, nW, self.num_heads, N, N) + mask.unsqueeze(1).unsqueeze(0)
-            attn = attn.view(-1, self.num_heads, N, N)
-            attn = self.softmax(attn)
-        else:
-            attn = self.softmax(attn)
+            attn_local = attn_local.view(B_ // nW, nW, self.num_heads, N, N) + mask.unsqueeze(1).unsqueeze(0)
+            attn_local = attn_local.view(-1, self.num_heads, N, N)
+            attn_global = attn_global.view(B_ // nW, nW, self.num_heads, N, N) + mask.unsqueeze(1).unsqueeze(0)
+            attn_global = attn_global.view(-1, self.num_heads, N, N)
 
+        attn_local = self.softmax(attn_local)
+        attn_global = self.softmax(attn_global)
+
+        attn = (attn_local + attn_global) / 2
         attn = self.attn_drop(attn)
 
         x = (attn @ v).transpose(1, 2).reshape(B_, N, C)
         x = self.proj(x)
         x = self.proj_drop(x)
         return x
+
 
 
 class SwinTransformerBlock(nn.Module):
